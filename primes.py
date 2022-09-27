4ef2dfb66b7f3d6c158f178fbb1f52a58e5fe54d
ACynthiaa <k20101949@kcl.ac.uk>
2022-09-27 20:26:36 +0800

    new version

--- primes.py
+++ primes.py
@@ -3,4 +3,15 @@
 
 def primes(number_of_primes):
     list = []
+    number=2
+    while len(list)<number_of_primes:
+        n=2
+        while n*n<number:
+            if number%n!=0:
+                n+=1
+            else:
+                break
+        if n*n>number:
+            list.append(number)
+        number+=1       
     return list
